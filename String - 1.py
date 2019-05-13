341ede7ec1ed44255e7fdb36ff1acc9faf138b5b
Esethu Jonas <jonas.gift83@gmail.com>
2019-05-13 17:34:07 +0200

    String - 1

---
+++ String - 1.py
@@ -0,0 +1,47 @@
+#String-1 > hello_name 
+def hello_name(name):
+  return "Hello " + name+"!"
+  
+#String-1 > make_abba
+def make_abba(a, b):
+  return a+(b)*2+a
+
+#String-1 > make_tags 
+def make_tags(tag, word):
+  return "<"+tag+">"+word+"</"+tag+">"
+  
+#String-1 > make_out_word 
+def make_out_word(out, word):
+  return out[:2]+word+out[2:]
+
+#String-1 > extra_end
+def extra_end(str):
+  return str[-2:]*3
+
+#String-1 > first_two 
+def first_two(str):
+  return str[:2]
+  
+#String-1 > first_half 
+def first_half(str):
+  half = int(len(str)/2)
+  return str[:half]
+
+#String-1 > without_end 
+def without_end(str):
+  return str[1:-1]
+
+#String-1 > combo_string 
+def combo_string(a, b):
+  if len(a) < len(b):
+    return a+b+a
+  else:
+    return b+a+b
+	
+#String-1 > non_start 
+def non_start(a, b):
+  return a[1:]+b[1:]
+
+#String-1 > left2 
+def left2(str):
+  return str[2:]+str[:2]
